882cf4e15f598a43156fb8886b22a61610b94d4e
Hoseung Lee <astrohsy@gmail.com>
2022-11-19 22:27:56 -0500

    apply black

--- src/routes/group.py
+++ src/routes/group.py
@@ -49,9 +49,7 @@ def create_group(group: GroupPostDto):
 
 
 @router.post("/{group_id}/members", response_model=GroupGetDto)
-def add_member_to_group(
-    new_member: MemberPostDto, group_id: int
-):
+def add_member_to_group(new_member: MemberPostDto, group_id: int):
     raise NotImplemented()
 
 
@@ -63,6 +61,7 @@ def delete_group(group_id: int):
 @router.delete("/{group_id}/members/{member_id}", response_model=GroupGetDto)
 def delete_member(group_id: int, member_id: int):
     raise NotImplemented()
+
 
 @router.put("/{group_id}", response_model=GroupGetDto)
 def edit_group(new_group: GroupPutDto, group_id: int):
