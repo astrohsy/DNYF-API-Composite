ddad3073d6b25092f78c8011da0876f7b86fcda6
Hoseung Lee <astrohsy@gmail.com>
2022-12-05 22:05:58 -0500

    generalized auth process

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -2,11 +2,8 @@
 
 
 # Third party imports
-from fastapi import APIRouter, Depends, Security, Request, HTTPException
+from fastapi import APIRouter, Request, HTTPException
 from authlib.integrations.starlette_client import OAuth
-from starlette.responses import RedirectResponse
-from authlib.integrations.starlette_client import OAuthError
-from fastapi.security import OAuth2PasswordBearer
 from starlette.config import Config
 import jwt
 
@@ -15,7 +12,6 @@ from src.config import settings
 
 # Scheme for the Authorization header
 # OAuth settings
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 config_data = {
     "JWT_SECRET_KEY": settings.jwt_secret_key,
     "GOOGLE_CLIENT_ID": settings.google_client_id,
@@ -30,18 +26,6 @@ oauth.register(
 )
 
 router = APIRouter(tags=["auth"])
-
-
-async def get_oauth_userinfo(token: str = Depends(oauth2_scheme)):
-    try:
-        return jwt.decode(
-            token,
-            settings.jwt_secret_key,
-            algorithms=["HS256"],
-            audience=settings.google_client_id,
-        )
-    except:
-        raise HTTPException(status_code=401, detail="Not Authorized")
 
 
 @router.get("/login")
---
+++ src/util/auth.py
@@ -0,0 +1,38 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from dataclasses import dataclass, fields
+import jwt
+
+# Local application imports
+from src.config import settings
+
+
+@dataclass
+class OAuthUserInfo:
+    email: str
+    email_verified: bool
+    exp: int
+    given_name: str
+    locale: str
+    name: str
+    picture: str
+    sub: str
+
+
+# OAuth settings
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+async def get_oauth_userinfo(token: str = Depends(oauth2_scheme)):
+    try:
+        decode = jwt.decode(
+            token,
+            settings.jwt_secret_key,
+            algorithms=["HS256"],
+            audience=settings.google_client_id,
+        )
+        field_names = [field.name for field in fields(OAuthUserInfo)]
+        filtered = {k: decode[k] for k in field_names}
+        return OAuthUserInfo(**filtered)
+    except:
+        raise HTTPException(status_code=401, detail="Not Authorized")
